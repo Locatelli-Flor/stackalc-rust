9bb42278f3e2df53fb2db3e522bad9e92a2258ae
Floriana Locatelli <floriana.locatelli@correo.ucu.edu.uy>
2025-04-14 18:51:08 -0300

    Agrego ceq clt cgt y cambios para tomar varios inputs por linea

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ use std::io;
 fn main() {
     let mut q: Vec<f64> = Vec::new();
 
-    print!("Enter input (q to quit): ");
+    println!("Enter input (q to quit): ");
     loop {
-
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
@@ -15,78 +14,118 @@ fn main() {
             break;
         }
 
-        let inst = parse(input);
-        perform(&mut q, inst);
+        let instructions: Vec<Instruction> = parse(input);
+        for instr in instructions {
+            perform(&mut q, instr);
+        }
     }
 
     println!("Goodbye!");
 }
 
-fn parse(input: &str) -> Instructions {
-    match input.trim().to_lowercase().as_str() {
-        s if s.starts_with("ldc ") => {
-            let num_str = &s[4..];
-            if let Ok(x) = num_str.trim().parse::<f64>() {
-                Instructions::Ldc(x)
-            } else {
-                Instructions::Unknown
+fn parse(input: &str) -> Vec<Instruction> {
+    let lis: Vec<&str> = input.split(' ').collect();
+    let mut results = Vec::new();
+
+    for instr in lis {
+        if instr.is_empty() {
+            continue;
+        }
+        let x = match instr.trim().to_lowercase().as_str() {
+            s if s.starts_with("ldc:") => {
+                let num_str = &s[4..];
+                if let Ok(x) = num_str.trim().parse::<f64>() {
+                    Instruction::Ldc(x)
+                } else {
+                    Instruction::Unknown
+                }
             }
-        },
-        "add" => Instructions::Add,
-        "sub" => Instructions::Sub,
-        "mul" => Instructions::Mul,
-        "div" => Instructions::Div,
-        "neg" => Instructions::Neg,
-        "ceq" => Instructions::Ceq,
-        "cgt" => Instructions::Cgt,
-        "clt" => Instructions::Clt,
-        "dup" => Instructions::Dup,
-        "pop" => Instructions::Pop,
-        _ => Instructions::Unknown
+            "add" => Instruction::Add,
+            "sub" => Instruction::Sub,
+            "mul" => Instruction::Mul,
+            "div" => Instruction::Div,
+            "neg" => Instruction::Neg,
+            "ceq" => Instruction::Ceq,
+            "cgt" => Instruction::Cgt,
+            "clt" => Instruction::Clt,
+            "dup" => Instruction::Dup,
+            "pop" => Instruction::Pop,
+            _ => Instruction::Unknown,
+        };
+        results.push(x);
     }
+    return results;
 }
 
-fn perform(q: &mut Vec<f64>, instr: Instructions) {
+fn perform(q: &mut Vec<f64>, instr: Instruction) {
     match instr {
-        Instructions::Ldc(x) => {
+        Instruction::Ldc(x) => {
             q.push(x);
         }
-        Instructions::Add | Instructions::Sub | Instructions::Mul | Instructions::Div => {
+        Instruction::Add | Instruction::Sub | Instruction::Mul | Instruction::Div => {
             if q.len() >= 2 {
-            let y = q.pop().unwrap();
-            let x = q.pop().unwrap();
-            let result = match instr {
-                Instructions::Add => x + y,
-                Instructions::Sub => x - y,
-                Instructions::Mul => x * y,
-                Instructions::Div => {
-                if y == 0.0 {
-                    eprintln!("Error: Division by zero.");
-                    q.push(x);
-                    q.push(y);
-                    return;
-                }
-                x / y
-                }
-                _ => unreachable!(),
-            };
-            q.push(result);
+                let y = q.pop().unwrap();
+                let x = q.pop().unwrap();
+                let result = match instr {
+                    Instruction::Add => x + y,
+                    Instruction::Sub => x - y,
+                    Instruction::Mul => x * y,
+                    Instruction::Div => {
+                        if y == 0.0 {
+                            eprintln!("Error: Division by zero.");
+                            q.push(x);
+                            q.push(y);
+                            return;
+                        }
+                        x / y
+                    }
+                    _ => unreachable!(),
+                };
+                q.push(result);
             } else {
                 print_error();
             }
-        },
-        Instructions::Neg => {
+        }
+        Instruction::Neg => {
             if q.len() >= 1 {
                 let x: f64 = q.pop().unwrap();
                 q.push(-x);
             } else {
                 print_error();
             }
-        },
-        Instructions::Ceq | Instructions::Cgt | Instructions::Clt => {
-
-        },
-        Instructions::Dup => {
+        }
+        Instruction::Ceq | Instruction::Cgt | Instruction::Clt => {
+            if q.len() >= 2 {
+                let x = q.pop().unwrap();
+                let y = q.pop().unwrap();
+                let result = match instr {
+                    Instruction::Ceq => {
+                        if x == y {
+                            1.0
+                        } else {
+                            0.0
+                        }
+                    }
+                    Instruction::Cgt => {
+                        if y > x {
+                            1.0
+                        } else {
+                            0.0
+                        }
+                    }
+                    Instruction::Clt => {
+                        if y < x {
+                            1.0
+                        } else {
+                            0.0
+                        }
+                    }
+                    _ => unreachable!(),
+                };
+                q.push(result);
+            }
+        }
+        Instruction::Dup => {
             if q.len() >= 1 {
                 let x: f64 = q.pop().unwrap();
                 q.push(x);
@@ -94,22 +133,22 @@ fn perform(q: &mut Vec<f64>, instr: Instructions) {
             } else {
                 print_error();
             }
-        },
-        Instructions::Pop => {
+        }
+        Instruction::Pop => {
             if q.len() >= 1 {
                 q.pop();
             } else {
                 print_error();
             }
-        },
-        Instructions::Unknown => {
+        }
+        Instruction::Unknown => {
             eprintln!("Unknown operation");
         }
     }
     println!("{:?}", q);
 }
 
-enum Instructions {
+enum Instruction {
     Ldc(f64),
     Add,
     Sub,
@@ -121,9 +160,9 @@ enum Instructions {
     Clt,
     Dup,
     Pop,
-    Unknown
+    Unknown,
 }
 
 fn print_error() {
-    eprintln!("Error: Not enough elements in the stack to perform operation."); 
+    eprintln!("Error: Not enough elements in the stack to perform operation.");
 }
